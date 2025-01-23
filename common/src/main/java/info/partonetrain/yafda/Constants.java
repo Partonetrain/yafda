94cfedade2404349772f735278cbf9bdc74c38e0
Partonetrain <Partonetrain@gmail.com>
2025-01-22 22:38:39 -0600

    prep

---
+++ common/src/main/java/info/partonetrain/yafda/Constants.java
@@ -0,0 +1,11 @@
+package info.partonetrain.yafda;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "examplemod";
+	public static final String MOD_NAME = "ExampleMod";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ fabric/src/main/java/info/partonetrain/yafda/platform/FabricPlatformHelper.java
@@ -0,0 +1,24 @@
+package info.partonetrain.yafda.platform;
+
+import info.partonetrain.yafda.platform.services.IPlatformHelper;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+}
