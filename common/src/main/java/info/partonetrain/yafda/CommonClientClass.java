00e4aa7b58908e143feb4fb006466933bd3ae37c
Partonetrain <Partonetrain@gmail.com>
2025-02-05 19:22:51 -0600

    backstab check

--- common/src/main/java/info/partonetrain/yafda/CommonClientClass.java
+++ common/src/main/java/info/partonetrain/yafda/CommonClientClass.java
@@ -3,14 +3,22 @@ package info.partonetrain.yafda;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.phys.Vec3;
 
 public class CommonClientClass {
     public static void flipRenderIfBackstabbable(PoseStack poseStack){
+        Minecraft minecraftInstance = Minecraft.getInstance();
         poseStack.scale(-1, -1, -1);
+        if(isAttackIndicatorShowing(minecraftInstance) && isBehindTarget(minecraftInstance)){
+            Constants.LOG.info("yes");
+        }
+        else{
+            Constants.LOG.info("no");
+        }
     }
 
-    public boolean isAttackIndicatorShowing(){
-        Minecraft minecraftInstance = Minecraft.getInstance();
+    public static boolean isAttackIndicatorShowing(Minecraft minecraftInstance){
+
         float f = minecraftInstance.player.getAttackStrengthScale(0.0F);
         boolean attackIndicator = false;
         if (minecraftInstance.crosshairPickEntity instanceof LivingEntity && f >= 1.0F) {
@@ -18,5 +26,15 @@ public class CommonClientClass {
             attackIndicator &= minecraftInstance.crosshairPickEntity.isAlive();
         }
         return attackIndicator;
+    }
+
+    //FD backstab check adapted to clientside
+    public static boolean isBehindTarget(Minecraft minecraftInstance) {
+        LivingEntity target = (LivingEntity) minecraftInstance.crosshairPickEntity;
+        Vec3 attackerLocation = minecraftInstance.player.position();
+        Vec3 lookingVector = target.getViewVector(1.0F);
+        Vec3 attackAngleVector = attackerLocation.subtract(target.position()).normalize();
+        attackAngleVector = new Vec3(attackAngleVector.x, 0.0D, attackAngleVector.z);
+        return attackAngleVector.dot(lookingVector) < -0.5D;
     }
 }
