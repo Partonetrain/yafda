dfa7b843da9fec1304a0fdb202de2d4b23941ca0
Partonetrain <Partonetrain@gmail.com>
2026-04-14 12:26:48 -0500

    finalize divineRPG integration

--- neoforge/src/main/java/info/partonetrain/yafda/integration/DivineRPGIntegration.java
+++ neoforge/src/main/java/info/partonetrain/yafda/integration/DivineRPGIntegration.java
@@ -1,11 +1,8 @@
 package info.partonetrain.yafda.integration;
 
-import com.kyanite.deeperdarker.util.DDTiers;
 import divinerpg.enums.ToolStats;
 import info.partonetrain.yafda.Constants;
 import info.partonetrain.yafda.YafdaNeoForge;
-import info.partonetrain.yafda.item.YafdaKnifeItem;
-import io.github.razordevs.deep_aether.item.gear.skyjade.SkyjadeWeapon;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
@@ -18,14 +15,13 @@ import net.neoforged.neoforge.registries.DeferredHolder;
 import vectorwing.farmersdelight.common.FoodValues;
 import vectorwing.farmersdelight.common.item.ConsumableItem;
 import vectorwing.farmersdelight.common.item.KnifeItem;
-import vectorwing.farmersdelight.common.registry.ModEffects;
 import vectorwing.farmersdelight.common.registry.ModItems;
 
 public class DivineRPGIntegration {
 
     public static final DeferredHolder<Item, Item> DIVINE_KNIFE = YafdaNeoForge.ITEMS.register(
             "divine_knife",
-            () -> new DivineKnifeItem(ToolStats.DIVINE_SWORD)
+            () -> new DivineKnifeItem(ToolStats.DIVINE_SHICKAXE)
     );
     public static final DeferredHolder<Item, Item> EMPOWERED_MEAT_SLICE = YafdaNeoForge.ITEMS.register(
             "empowered_meat_slice",() -> new ConsumableItem(ModItems.foodItem(FoodValues.MINCED_BEEF)
@@ -34,17 +30,18 @@ public class DivineRPGIntegration {
             "cooked_empowered_meat_slice",() -> new ConsumableItem(ModItems.foodItem(FoodValues.BEEF_PATTY)
             ));
 
-    public static final FoodProperties EVERYTHING_BAGEL_PROPERTIES = (new FoodProperties.Builder()).nutrition(14).saturationModifier(0.75F).effect(() -> {
-        return FoodValues.nourishment(6000);
-    }, 1.0F).effect(() -> {
-        return new MobEffectInstance(MobEffects.ABSORPTION, 6000, 2, false, false);
-    }, 1.0F).effect(() -> {
-        return new MobEffectInstance(MobEffects.DAMAGE_BOOST, 6000, 0, false, false);
-    }, 1.0F).alwaysEdible().build();
+//    public static final FoodProperties EVERYTHING_BAGEL_PROPERTIES = (new FoodProperties.Builder()).nutrition(14).saturationModifier(0.75F).effect(() -> {
+//        return FoodValues.nourishment(6000);
+//    }, 1.0F).effect(() -> {
+//        return new MobEffectInstance(MobEffects.ABSORPTION, 6000, 2, false, false);
+//    }, 1.0F).effect(() -> {
+//        return new MobEffectInstance(MobEffects.DAMAGE_BOOST, 6000, 0, false, false);
+//    }, 1.0F).alwaysEdible().build();
+//
+//    public static final DeferredHolder<Item, Item> EVERYTHING_BAGEL = YafdaNeoForge.ITEMS.register(
+//            "everything_bagel",() -> new ConsumableItem(ModItems.foodItem(EVERYTHING_BAGEL_PROPERTIES).rarity(Rarity.UNCOMMON)
+//            , true));
 
-    public static final DeferredHolder<Item, Item> EVERYTHING_BAGEL = YafdaNeoForge.ITEMS.register(
-            "everything_bagel",() -> new ConsumableItem(ModItems.foodItem(EVERYTHING_BAGEL_PROPERTIES).rarity(Rarity.UNCOMMON)
-            , true));
     public static void load() {
         Constants.LOG.info("DivineRPG integration loaded");
     }
@@ -55,7 +52,7 @@ public class DivineRPGIntegration {
         }
 
         public static Item.Properties CreateKnifeProperties(Tier tier) {
-            return (new Item.Properties().component(DataComponents.UNBREAKABLE, new Unbreakable(true))).attributes(KnifeItem.createAttributes(tier, -2.0F, -2.0F));
+            return (new Item.Properties().component(DataComponents.UNBREAKABLE, new Unbreakable(true))).attributes(KnifeItem.createAttributes(tier, -1.0F, -2.0F));
         }
     }
 }
